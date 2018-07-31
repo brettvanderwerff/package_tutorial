642d07920145c2a8068bac4df59f65b6fafceade
Brett.Vanderwerff <vande060@gmail.com>
2018-07-30 23:52:20 -0700

    add if __name__ == __main__

--- pypi_tutorial/__main__.py
+++ pypi_tutorial/__main__.py
@@ -1,9 +1,10 @@
-from pypi_tutorial import command_line
-from pypi_tutorial import my_prog # why odes this not work
+from pypi_tutorial.command_line import parse_args
+from pypi_tutorial.my_prog import compare_num
 
 
 def main():
-    num1, num2 = command_line.parse_args()
-    my_prog.compare_num(num1, num2)
+    num1, num2 = parse_args()
+    compare_num(num1, num2)
 
-main()
+if __name__ == '__main__':
+    main()

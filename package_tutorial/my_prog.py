51016c6f9858bd3cffca1c70aacd1bd60cca8cd6
brett vanderwerff <vande060@gmail.com>
2018-08-12 19:14:18 -0700

    changed my_prog function name to run

--- package_tutorial/my_prog.py
+++ package_tutorial/my_prog.py
@@ -1,7 +1,7 @@
 from package_tutorial.get_average import get_average
 from package_tutorial.get_sum import get_sum
 
-def my_prog(input_list):
+def run(input_list):
     get_average.run(input_list=input_list)
     get_sum.run(input_list=input_list)
 
